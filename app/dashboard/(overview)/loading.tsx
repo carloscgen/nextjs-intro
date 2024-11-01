0940b8c726229c63c3d2106321a9332ee927e1b4
Carl <carloscgentile@gmail.com>
2024-11-01 16:18:37 -0300

    search and pagination

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+// este componente se carga automaticamente cuando se esta cargando la data en la pagina
+import DashboardSkeleton from '@/app/ui/skeletons';
+ 
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
